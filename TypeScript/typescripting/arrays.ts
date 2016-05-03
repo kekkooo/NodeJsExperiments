909c90214628f345c240bd6593a101d6746ffa58
kekkooo <fr.usai1@gmail.com>
2016-05-03 11:43:52 +0200

    one more.

---
+++ TypeScript/typescripting/arrays.ts
@@ -0,0 +1,16 @@
+/**
+ * Startup
+ */
+class Startup {
+    public static main() :number{
+        var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+        /*var limit = 10;
+        for( var i = 0; i < limit; ++i ){
+            total += i;
+        } */       
+        console.log( pizzaToppings );
+        return 0;        
+    }
+}
+
+Startup.main();
