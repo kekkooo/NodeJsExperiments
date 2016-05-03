6c7617b52118092cea7c83f7076bba412ed1c397
kekkooo <fr.usai1@gmail.com>
2016-05-03 12:37:42 +0200

    completed the javascripting tutorial in Typescript.. hooray!

---
+++ TypeScript/typescripting/function_parameters.ts
@@ -0,0 +1,8 @@
+class Startup {
+    public static main() :number{        
+        var math = (a:number, b:number, c:number) => (b*c) + a;
+        console.log( math(53, 61, 67) );
+        return 0;        
+    }
+}
+Startup.main();
---
+++ TypeScript/typescripting/functions.ts
@@ -0,0 +1,8 @@
+class Startup {
+    public static main() :number{        
+        var eat = (food:string ) => food + ' tasted really good.';
+        console.log( eat('bananas') );
+        return 0;        
+    }
+}
+Startup.main();
