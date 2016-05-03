f372d4fe3a119a58fcb8d3c7411242de3f5982a9
kekkooo <fr.usai1@gmail.com>
2016-05-03 11:49:09 +0200

    too much copy&paste

--- TypeScript/typescripting/arrays.ts
+++ TypeScript/typescripting/arrays.ts
@@ -4,10 +4,6 @@
 class Startup {
     public static main() :number{
         var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
-        /*var limit = 10;
-        for( var i = 0; i < limit; ++i ){
-            total += i;
-        } */       
         console.log( pizzaToppings );
         return 0;        
     }
